1d577696287941d75b7c2b675622b27f6724fbd5
ishubham21 <sg2199203@gmail.com>
2022-08-12 08:41:50 +0530

    vercel.json and vercel.sh added

--- src/routes/index.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import createHttpError from "http-errors";
-
-const router: Router = Router();
-
-router.get("/", (req, res) => {
-  res.send("Hello world!");
-});
